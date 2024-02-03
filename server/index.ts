28592cb3723deb04d4717c93fe2190e89a828728
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-02-03 16:51:00 +0530

    updated

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,10 @@ import { ListenPlugin, RouterPlugin } from "./plugins";
 
 const app = express();
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", (req, res) => {
+  res.json({ success: true, message: "Success" });
+});
 RouterPlugin.routeSetup(app);
 ListenPlugin.listen(app);
